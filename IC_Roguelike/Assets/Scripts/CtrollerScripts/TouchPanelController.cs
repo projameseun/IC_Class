be93eb682ca52a7d60d37a6331c3d3c6ebff2264
tlawowns7 <tlawowns78@naver.com>
2020-06-20 00:12:05 +0900

    플레이어 드래그이동

--- IC_Roguelike/Assets/Scripts/CtrollerScripts/TouchPanelController.cs
+++ IC_Roguelike/Assets/Scripts/CtrollerScripts/TouchPanelController.cs
@@ -3,28 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TouchPanelController : MonoBehaviour, IDragHandler
+public class TouchPanelController : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
     public GameObject playerObject;
     Vector2 touchPos;
-    float deltaX, deltaY;
 
     public void OnDrag(PointerEventData eventData)
     {
+        Vector2 worldPoint = Input.mousePosition;
+        worldPoint = Camera.main.ScreenToWorldPoint(worldPoint);
+
+        Vector2 diffPos = worldPoint - touchPos;
+
+        touchPos = Input.mousePosition;
         touchPos = Camera.main.ScreenToWorldPoint(touchPos);
-        Debug.Log(touchPos);
+
+        playerObject.transform.position = new Vector2(playerObject.transform.position.x + diffPos.x,
+                                                        playerObject.transform.position.y + diffPos.y);
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
-    }
+        touchPos = Input.mousePosition;
+        touchPos = Camera.main.ScreenToWorldPoint(touchPos);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Debug.Log("touchPos : " + touchPos);
     }
 }
